ead33c7233377160db9bfdd81fa49b0b7ad7a92a
JeanBilheux <bilheuxjm@ornl.gov>
2011-11-29 14:49:22 -0500

    showroom onglets now move to the left as expected when mouseover.

--- scripts/showroom.js
+++ scripts/showroom.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	
 	// $("#showroom_right a").blend();	
 		
-	$('#showroom_right>a').mouseover(function() {
+	$('#showroom_right>div>a').mouseover(function() {
 		// $('#showroom_right a').addClass('showroom_not_hover');
 		$(this).addClass('showroom_hover');
 	});
 	
-	$('#showroom_right>a').mouseover(function() {
+	$('#showroom_right>div>a').mouseover(function() {
 		// $('#showroom_right a').addClass('showroom_not_hover');
-		$('#showroom_right>a').removeClass('showroom_hover');
+		$('#showroom_right>div>a').removeClass('showroom_hover');
 		$(this).addClass('showroom_hover');
 	});
 
