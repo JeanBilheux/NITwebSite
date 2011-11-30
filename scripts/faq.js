2395cd30e2392664bee7b3e44b0ff0e84efd3c21
JeanBilheux <bilheuxjm@ornl.gov>
2011-11-30 15:55:01 -0500

    Added hide or not of the answer div

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,26 +1,23 @@
-$(document).ready(function(){
-  $('#faq_div ul > li ul')
-    .click(function(e){
-      e.stopPropagation();
-    })
-    // .filter(':not(:first)')
-    .hide();
-    
-  $('#faq_div ul > li').click(function(){
-    var selfClick = $(this).find('ul:first').is(':visible');
-    if(!selfClick) {
-      $(this)
-        .parent()
-        .find('> li ul:visible')
-        .slideToggle();
-    }
-    
-    $(this)
-      .find('ul:first')
-      .stop(true, true)
-      .slideToggle();
-  });
-  
-  $('#answers').hide(); //at the start, hidden
-  
+$(document).ready(function() {
+	$('#faq_div ul > li ul').click(function(e) {
+		e.stopPropagation();
+	})
+	// .filter(':not(:first)')
+	.hide();
+
+	$('#faq_div ul > li').click(function() {
+		var selfClick = $(this).find('ul:first').is(':visible');
+		if(!selfClick) {
+			$(this).parent().find('> li ul:visible').slideToggle();
+			$('#answers').show();
+		} else {
+			$('#answers').hide();
+		}
+
+		$(this).find('ul:first').stop(true, true).slideToggle();
+	});
+
+	$('#answers').hide();
+	//at the start, hidden
+
 });
