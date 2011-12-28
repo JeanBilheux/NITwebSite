2d1e56926c19cf9fa3445c9b32a37065b8fab3b1
Jean Bilheux <bilheuxjm@ornl.gov>
2011-12-28 11:38:16 -0500

    Implemented first part of publications.

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,26 +1,6 @@
 $(document).ready(function() {
-	// $('#faq_div ul > li ul')
-	// .click(function(e) {
-	// e.stopPropagation();
-	// })
-	// // .filter(':not(:first)')
-	// .hide();
-	//
-	// $('#faq_div > ul > li, #faq_div ul > li > ul > li').click(function() {
-	// var selfClick = $(this).find('ul:first').is(':visible');
-	// if(!selfClick) {
-	// $('#faq_div > ul > li').css('background-position', 'right top');
-	// $(this).parent().find('> li ul:visible').slideToggle();
-	// $('#answers').show();
-	// // $(this).css('background-position', 'right -20px');
-	// } else {
-	// $('#answers').hide();
-	// // $(this).css('background-position', 'right top');
-	// }
-	// $(this).find('ul:first').stop(true, true).slideToggle();
-	// });
-
-	$('#accordion').accordion({
+	
+	$('#accordion_faq').accordion({
 		header : 'h3'
 	});
 
@@ -28,16 +8,4 @@ $(document).ready(function() {
 		alert("I have been clicked");
 	});
 	
-	// $('#accordion > h3').click(function() {
-	// var selfClick = $(this).find('ul:first').is(':visible');
-	// if(!selfClick) {
-	// $('#answers').show();
-	// } else {
-	// $('#answers').hide();
-	// }
-	// });
-	//
-	// $('#answers').hide();
-	//at the start, hidden
-
 });
