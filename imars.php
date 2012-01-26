3616c9ed9a0a86eb2c19e16ef0acff965718f388
Jean Bilheux <bilheuxjm@ornl.gov>
2012-01-26 10:03:16 -0500

    Added temporaty iMars page

--- imars.php
+++ imars.php
@@ -1,86 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-		<title>ORNL Neutron Imaging Teame</title>
-		<link rel="stylesheet" type="text/css" href="styles/index.css" />
-		<link rel="stylesheet" type="text/css" href="styles/jquery-ui-1.7.2.custom.css" />
-		<link rel="stylesheet" type="text/css" href="styles/tabs.css" />
-		<link rel="stylesheet" type="text/css" href="styles/imars.css" />
-	</head>
-	<body>
-		<div id="container">
-			<div id="header">
-				<div id="menu">
-					<!--Add the interactive menu here -->
-					<ul id="nav-shadow">
-						<li class="button-color-1">
-							<a href="publications.php" title="Publications"></a>
-						</li>
-						<li class="button-color-2">
-							<a href="faq.php" title="Q&As"></a>
-						</li>
-						<li class="button-color-3">
-							<a href="multimedia.php" title="Multimedia"></a>
-						</li>
-						<li class="button-color-4">
-							<a href="team.php" title="Meet the team">Meet the team</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div id="contact_top">
-				<a href="index.php"><img src='images/home.png' alt='Home' align=center width="30" /></a>
-			</div>
-			
-						<div id="spacer">
-			</div>
-			
-			<div id="content">
-				<div class="article">
-					<p>
-						<a id="top_of_page" href="#">Top of page</a>
-					</p>
-				</div>
-				<br/>
-				<hr>
-				<div id="contact">
-						<a id="ornl_link" href="http://www.ornl.gov/"></a>
-						<a id="us_energy_link" href="http://science.energy.gov/"></a>
-						<!-- 				<a href="index.html"><img src='images/home.png' alt='Home' align=center width="30" /></a> -->
-						<!-- 				&nbsp;&nbsp;&nbsp;<b>-</b>&nbsp;&nbsp;&nbsp; -->
-						<a href="contact_us.php">Contact us</a> | <a href="http://www.ornl.gov/ornlhome/disclaimrs.shtml">Security Notice</a> | <a href="http://www.oakridge.doe.gov/external/">DOE</a> | <a href="http://ut-battelle.org/">UT-Battelle</a>
-						<br/>
-						<br/>
-						<div id="bottom_message">
-							SNS and HFIR are Office of Science user facilities operated for the <a href="http://energy.gov/">U.S. Department of Enegy</a>
-							<a href="http://www.science.doe.gov/"> Office of Science</a>
-							<br/>
-							by <a href="http://www.ornl.gov/">Oak Ridge National Laboratory</a>, which is managed by the <a href="http://www.ut-battelle.org/">UT-Battelle, LLC</a>.
-					<br/>
-						<br/>
-						<span id="last_update">Last updated: <?php require_once('appvars.php'); echo LAST_UPDATE
-							?></span>
-						</div>
-						<br/>
-						<br/>
-						<br/>
-					</div>
-			</div>
-			<script src="scripts/jquery.js" type="text/javascript"></script>
-			<script src="scripts/animated_background.js" type="text/javascript"></script>
-			<script src="scripts/jquery-1.3.2.min.js" type="text/javascript"></script>
-			<script src="scripts/imars_scripts.js" type="text/javascript"></script>
-			<script src="scripts/menu_buttons.js" type"text/javascript"></script>
-			<script src="scripts/jquery.easing.1.3.js" type="text/javascript"></script>
-			<script src="scripts/jquery.kwicks-1.5.1.pack.js" type="text/javascript"></script>
-			<script src="scripts/tab_scripts.js" type="text/javascript"></script>
-			<script src="scripts/jquery.blend-min.js" type="text/javascript"></script>
-			<script src="scripts/jquery.hoverIntent.minified.js" type="text/javascript"></script>
-			<script src="scripts/jquery-ui-1.7.2.custom.min.js" type="text/javascript"></script>
-			<script src="scripts/jquery.scrollTo.js" type="text/javascript" charset="utf-8"></script>
-			<script src="scripts/bottom_right.js" type="text/javascript"></script>
-			<!-- 		<script src="scripts/showroom.js" type="text/javascript"></script> -->
-	</body>
-</html>
+<?php
+require_once ('header_part1.php');
+?>
+<link rel="stylesheet" type="text/css" href="styles/imars.css" />
+<style type="text/css">
+	#container {
+		height: 935px;
+	}
+	#body {
+		height: 1000px;
+	}
+</style>
+<?php
+require_once ('header_part2.php');
+?>
+<div id="content">
+	<div class="article">
+		<img src="images/coming_soon_imars.png" width="100%" alt="Coming soon iMars description"/>
+		
+		
+		<p>
+<!-- 			<a id="top_of_page" href="#">Top of page</a> -->
+		</p>
+	</div>
+</div>
+<br/>
+<hr width="90%">
+<?php
+require_once ('footer_part1.php');
+?>
+
+<script src="scripts/ejectors_scripts.js" type="text/javascript"></script>
+</body>
+</html> 
